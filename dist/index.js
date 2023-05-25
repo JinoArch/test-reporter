eca83e39d1cb22d5dda95e39591fba4d0f22ce5e
rr-jino-jose <jjose@ebates.com>
2023-05-25 23:31:17 +0530

    ✨ feat: update TestReporter to output check_run_html_url without quotes
    This commit updates the `TestReporter` class to output `check_run_html_url` without quotes. Before this change, `core.setOutput('check_run_html_url', JSON.stringify(`${resp.data.html_url}`))` was used which resulted in the URL being surrounded by quotes. With this change, `core.setOutput('check_run_html_url', `${resp.data.html_url}`)` is used instead, resulting in the URL being output without quotes.

--- dist/index.js
+++ dist/index.js
@@ -411,7 +411,7 @@ class TestReporter {
             core.info(`Check run create response: ${resp.status}`);
             core.info(`Check run URL: ${resp.data.url}`);
             core.info(`Check run HTML: ${resp.data.html_url}`);
-            core.setOutput('check_run_html_url', JSON.stringify(`${resp.data.html_url}`));
+            core.setOutput('check_run_html_url', `${resp.data.html_url}`);
             return results;
         });
     }
